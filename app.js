5a3ed15c27f44179392a6c5d54d6f39fcb923ee3
Develop-RH <rmghazii1@gmail.com>
2024-09-14 11:39:30 +0100

    pictures

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-document.addEventListener('DOMContentLoaded' , () => {
+function showImage(imageSrc) {
+    document.getElementById('displayed-image').src = imageSrc;
+}
 
-    const images = document.querySelectorAll('img');
-
-    for(const image of images){
-        fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 100;
-            image.height = 100;
-        })
-
-    }
-})
